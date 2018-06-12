184420fa0e8f512c13694f5f6862d8b4de92842e
Prabald <36597875+Prabald@users.noreply.github.com>
2018-06-12 18:32:56 +0530

    Seperated scripts and css

---
+++ script.js
@@ -0,0 +1,61 @@
+var i=0;
+             
+                var start = new Date().getTime();
+                document.getElementById("shape").onclick=function()
+                {
+                    document.getElementById("shape").style.opacity=0;
+                    var end=new Date().getTime();
+                    document.getElementById("secs").innerHTML=(end-start)/1000;  
+                    tout();
+                   
+                     
+                } 
+            function tout()
+            {
+                setTimeout(makeShapeAppear,Math.random()*2000);
+            }
+            
+            
+            function makeShapeAppear()
+            {
+                var top=Math.random()*400;
+                var left=Math.random()*400;
+                var width=Math.random()*300;
+                 document.getElementById("shape").style.top=top+"px";
+                document.getElementById("shape").style.left=left+"px";
+                document.getElementById("shape").style.width= width+"px";
+                document.getElementById("shape").style.height=width+"px";
+                document.getElementById("shape").style.opacity=1;
+                document.getElementById("shape").style.backgroundColor=getRandomColor();
+                getRandomShape();
+                start= new Date().getTime();
+
+            }
+         
+            
+            
+            function getRandomColor()
+            {
+                var no= "0123456789ABCDEF".split('');
+                var color='#';
+                for(var i=0;i<6;i++)
+                    {
+                        color=color+no[Math.ceil(Math.random()*16)];
+                    }
+                return color;
+                
+            }
+            
+            function getRandomShape()
+            {
+                var r=Math.floor(Math.random()*100);
+                if (r%2==0)
+                        document.getElementById("shape").style.borderRadius="100%";
+                    
+            }
+            
+            
+            
+            
+            
+            
